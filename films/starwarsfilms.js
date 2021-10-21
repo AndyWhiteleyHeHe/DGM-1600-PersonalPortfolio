48242b1d9d2ae01df0c1bf87b05645ab6d6f54c4
AndyWhiteleyHeHe <91302285+AndyWhiteleyHeHe@users.noreply.github.com>
2021-10-20 19:07:11 -0600

    2nd commit woohoo

---
+++ films/starwarsfilms.js
@@ -0,0 +1,8 @@
+import {films} from '../data/films.js'
+
+let filmList = document.querySelector('#filmlist')
+
+for (let i = 0' i < films.length; i++) {
+    let figure = document.createElement('figure')
+    let figure
+}
