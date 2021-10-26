1ede499623d6e88c2b75d9f6850029789d5096e4
AndyWhiteleyHeHe <91302285+AndyWhiteleyHeHe@users.noreply.github.com>
2021-10-26 11:16:08 -0600

    4th commit ye ye

--- characters/swcharacters.js
+++ characters/swcharacters.js
@@ -6,8 +6,14 @@ const mainBody = document.querySelector('body')
 const header = document.createElement)('header')
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
+
 const femaleButton = document.createElement('button')
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 femaleButton.textContent = 'Female Characters'
+
 header.appendChild(maleButton)
 header.appendChild(femaleButton)
 
@@ -15,15 +21,21 @@ document.body.insertBefore(header, mainContent)
 
 const maleCharacters = people.filter{person => person.gender === 'male'}
 const femaleCharacters = people.filter {person => person.gender === 'female'}
+const otherCharcters = people.filter{(person) => 
+    if gender === 'hermaphrodite'
+    || person.gender === 'n/a'
+    || person.gender === 'none'    
+}
 
-console.log(maleCharacters)
+function populationDOM(characters) {
 
-people.forEach(element => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    if(index !== 16) {
-        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`
+    people.forEach(element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        if(index !== 16) {
+            charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`
         }
+    }
 
 const charCaption = document.createElement('figcaption')
 charCaption.textContent = `${element.name} at index ${index}`
@@ -32,3 +44,14 @@ charFigure.appendChild(charImg)
 charFigure.appendChild(charCaption)
 charFigure.appendChild(CharacterData)
 })
+
+function findLastNumber(url){
+    let end = url,lastIndexOf('/')
+    let start = end =2 
+    console.log(url.charAt(start))
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+cgetLastNumber('https://swapi.co/api/people/18/')
