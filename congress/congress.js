78933dd62452aa48d843c4ee9dcf8808e991d202
AndyWhiteleyHeHe <91302285+AndyWhiteleyHeHe@users.noreply.github.com>
2021-11-17 23:53:10 -0700

    updates to congress

--- congress/congress.js
+++ congress/congress.js
@@ -1,20 +1,28 @@
-import { seantors } from '../data/senators.js'
+import { senators } from '../data/senators.js'
+import { representatives } from '../data/senators.js'
 
-const senatorDiv = document.querySelector('.senators')
+const members = [...senators, ...representatives]
 
-function SimplifiedSenators(senatorArray) {
-    return senatorArray.map(senator => {
+function SimplifiedMembers(chamberFilter) {
+    const filteredArray = members.filter((member) =>
+        chamberFilter ? member.short_title === chamberFilter : member,)
+    
+    return filteredArray.map((senator) => {
         let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
         return {
             id: senator.id,
             name: `${senator.first_name}${middleName}${senator.last_name}`,
             party: senator.party
+            missedVotesPct: senator.missed_votes_pct,
+            loyaltyPct: senator.votes_with_party_pct,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
         }
     })
 }
 
-populateSenatorDiv(SimplifiedSenators(senators))
+const senatorDiv = document.querySelector('.senators')
+const loyaltyHeading = document.querySelector('.mostLoyal')
+const seniorityHeading = document.querySelector('.seniority')
 
 function populateSenatorDiv(simpleSenators) {
     simpleSenators.forEach(senator => {
@@ -39,8 +47,26 @@ console.log(filterSenators('party','R'))
 
 const filterSenators = (prop,value) => SimplifiedSenators
 
-const mostSeniorSenator = SimplifiedSenators().reduce()
+const mostSeniorSenator = SimplifiedSenators().reduce((acc, senator) => 
+acc.seniority > senator.seniority ? acc : senator,)
 
+//seniorityHeading.textContent = `The oldest member of Congress is ${mostSeniorMember.name} who has served in Congress since ${mostSeniorMember.year}`
 
+const mostLoyal = SimplifiedMembers().reduce((acc, senator) => {
+    if (senator.loyaltyPct === 100) {
+        acc.push(senator)
+    }
+    return acc
+}, [])
 
-populateSenatorDiv(SimplifiedSenators{senators})
+const traitorList = document.createElement('ol')
+
+const snake = mostLoyal.map((traitor) => {
+    let listItem = document.createElement('li')
+    listItem.textContent = coward.name
+    cowardList.appendChild(listItem)
+})
+
+loyaltyHeading.appendChild(traitorList)
+
+populateSenatorDiv(SimplifiedMembers())
